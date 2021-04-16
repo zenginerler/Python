1231f5b4cd456d1b566e26a2b21ed396b6fe5cf2
zenginerler <yavuzenginerler@gmail.com>
2021-04-15 21:33:40 -0500

    add lecture 6 notes

---
+++ tutorial_6.py
@@ -0,0 +1,24 @@
+"""
+Function Call Practice
+"""
+
+def add(x, y):
+    return x + y
+
+def substract(x, y):
+    return x - y
+
+
+def main():
+    
+    # call the function add()
+    result1 = add(10, 20) 
+    # call the function substract()
+    result2 = substract(20, 10)
+
+    # print the result
+    print(result1 , result2)
+
+
+main()
+    
